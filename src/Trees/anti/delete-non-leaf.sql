f4e42cca723c01581cdf0de544abff1fcd89f8d4
古川広一 <ex.kouichi1224@gmail.com>
2021-04-30 23:23:29 +0900

    add

---
+++ src/Trees/anti/delete-non-leaf.sql
@@ -0,0 +1,22 @@
+USE trees;
+
+---------------------------------------------------
+-- mysql> select id, parent_message_id as parent from Message;
+-- +----+--------+
+-- | id | parent |
+-- +----+--------+
+-- |  1 |   NULL |
+-- |  2 |      1 |
+-- |  4 |      1 |
+-- |  3 |      2 |
+-- |  5 |      4 |
+-- |  6 |      4 |
+-- |  7 |      6 |
+-- +----+--------+
+-- 
+-- id = 6 を削除する場合、parent_message_id = 6 のノードの parent_message_id を 4 に変更する必要がある
+---------------------------------------------------
+
+SELECT parent_message_id FROM Message WHERE id = 6; -- 4 を返す
+UPDATE Message SET parent_message_id = 4 WHERE parent_message_id = 6;
+DELETE FROM Message WHERE id = 6;
